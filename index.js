94e086380b53062ce48cb89e8fb3cbbd19fab82a
shiv pratap singh <pratapshiv1992@gmail.com>
2019-02-17 15:13:50 +0530

    a rest API with query string demo

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/test',(req,res)=> {
     res.send(`Hi it's ${new Date().toUTCString()}`);
 });
 
+// /query?name=xyz
+app.get('/query',(req,res)=> {
+    const params = JSON.stringify(req.query);
+    res.send(`you send me ${(params)}`);
+});
+
 app.listen(port,()=>{
     console.log('server running at port ');
 })
